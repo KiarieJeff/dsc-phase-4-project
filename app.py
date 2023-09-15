ceb54ac41a9f24df03ce6be624cbdfaa05f52a32
J.Kiarie <mjeffakama@gmail.com>
2023-09-15 14:25:17 +0300

    making other changes

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ from nltk.tokenize import word_tokenize,TweetTokenizer
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import re
+import numpy as np
+from scipy.sparse import csr_matrix
 
 
 app = Flask(__name__)
 
 # Load the trained model
-with open('model_rf.pkl', 'rb') as model_file:
+with open('sent_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
+
+with open('tfidf_vectorizer.pkl', 'rb') as vect_file:
+    vectorizer = pickle.load(vect_file)    
 
 def clean_and_preprocess_text(text):
     tokenizer = TweetTokenizer()
@@ -31,24 +36,34 @@ def clean_and_preprocess_text(text):
     cleaned_text = ' '.join(stemmed_tokens) 
     return cleaned_text
 
-def map_to_new_column(value):
-    if value == 0.0:
-        return 'negative'
-    elif value == 1.0:
-        return 'positive'
-    elif value == 2.0:
-        return 'neutral'
+def sentiment_predict(text):
+    cl_txt = clean_and_preprocess_text(text)
+    tfidf_vector_single = vectorizer.transform([cl_txt])
+    csr_mat = csr_matrix(tfidf_vector_single)
+    csr_array = csr_mat.toarray()
+    pred = model.predict(csr_array)
+    rounded_arr = np.round(pred)
+
+    # Extract the rounded values 
+    a, b, c = rounded_arr[0]
+
+    # Determine the sentiment label based on the rounded values
+    if a == 1:
+        return "Negative"
+    elif b == 1:
+        return "Positive"
+    elif c == 1:
+        return "Neutral"
     else:
-        return 'unknown'  
+        return "Unknown"
+  
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json()
         x = data['x']  # Assuming you send a JSON request with 'x' as input
-        processed_text = clean_and_preprocess_text(x)
-        prediction = model.predict([processed_text])
-        prediction = map_to_new_column(prediction[0])
+        prediction = sentiment_predict(x)
         response = {'prediction': prediction}
         return jsonify(response)
     except Exception as e:
